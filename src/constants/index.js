63832d81fd170bce072f4f10e42e418ab376552e
j27000 <namanjhala32@gmail.com>
2024-09-13 18:30:59 -0400

    index change

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,46 +1,27 @@
 import {
   mobile,
-  backend,
-  creator,
   web,
   javascript,
   html,
   css,
   reactjs,
+  notion,
   redux,
   tailwind,
   git,
-  figma,
-  docker,
-  chainlit,
-  aws,
-  valuelabs,
-  tcr,
   circle,
-  fakeshop,
   cpp,
-  java,
   mysql,
   dbms,
   github,
-  bitbucket,
   vscode,
   postman,
   firebase,
-  appwrite,
   ts,
-  next,
   eclipse,
-  jira,
-  miro,
-  sentry,
-  notion,
-  aws2,
-  food2,
-  sakshi,
-  urvi,
-  sahil,
-  nike,
+  petadopt,
+  library,
+  gameStreet,
 } from "../assets";
 
 export const navLinks = [
@@ -71,18 +52,7 @@ const services = [
     title: "Frontend Developer",
     icon: mobile,
   },
-  {
-    title: "Java Developer",
-    icon: backend,
-  },
-  // {
-  //   title: "Data Structure & Algorithms",
-  //   icon: creator,
-  // },
-  {
-    title: "RPA Developer",
-    icon: creator,
-  },
+
 ];
 
 const frontendTechnologies = [
@@ -93,10 +63,6 @@ const frontendTechnologies = [
   {
     name: "Redux",
     icon: redux,
-  },
-  {
-    name: "Next Js",
-    icon: next,
   },
 
   {
@@ -130,31 +96,12 @@ const tools = [
     name: "github",
     icon: github,
   },
-  {
-    name: "Sentry",
-    icon: sentry,
-  },
-  {
-    name: "figma",
-    icon: figma,
-  },
+
   {
     name: "VS Code",
     icon: vscode,
   },
-  {
-    name: "docker",
-    icon: docker,
-  },
 
-  {
-    name: "Jira",
-    icon: jira,
-  },
-  {
-    name: "Miro",
-    icon: miro,
-  },
   {
     name: "Postman",
     icon: postman,
@@ -168,20 +115,13 @@ const tools = [
     name: "Notion",
     icon: notion,
   },
-  {
-    name: "Bitbucket",
-    icon: bitbucket,
-  },
+
 ];
 
 const otherTechnologies = [
   {
     name: "C++",
     icon: cpp,
-  },
-  {
-    name: "Java",
-    icon: java,
   },
   {
     name: "Mysql",
@@ -192,225 +132,175 @@ const otherTechnologies = [
     icon: firebase,
   },
   {
-    name: "AWS",
-    icon: aws2,
-  },
-  {
-    name: "appwrite",
-    icon: appwrite,
-  },
-  {
     name: "DBMS",
     icon: dbms,
-  },
-  {
-    name: "Chainlit",
-    icon: chainlit,
   },
 ];
 
 const experiences = [
   {
-    title: "Software Engineer Specialist",
-    company_name: "Valuelabs, HYD, India",
-    icon: valuelabs,
+    title: "Petadopt - Full-Stack Web Application for Pet Adoption",
+    company_name: "Personal Project",
+    icon: petadopt,
     iconBg: "#383E56",
-    date: "Jan 2023 - Present",
+    date: "June 2024 - August 2024",
     points: [
-      "Developing and maintaining web applications using React.js and other related technologies.",
-      "Developed RPA flow and AI model using UiPath & MS Power Automate",
-      "Collaborating with cross-functional teams including, product managers, and other developers to create high-quality products.",
-      "Implementing responsive design and ensuring cross-browser compatibility.",
-      "Participating in code reviews and providing constructive feedback to other developers.",
+      "AI-Driven Pet Matching: Integrated Firebase Chatbot with PaLM AI to accurately match pets with potential adopters.",
+      "Real-Time Data Management: Leveraged Firebase Firestore for high uptime and fast data retrieval, ensuring a seamless user experience.",
+      "Advanced Pet Listing and Search: Developed a robust listing system with advanced search capabilities to enhance user engagement.",
+      "Admin Dashboard and Shopping Cart: Built an intuitive admin dashboard and introduced a shopping cart with personalized product recommendations.",
+      "Optimized Performance: Utilized React Context API and React Router to improve application performance and navigation.",
+      "Launched an innovative shopping cart solution that enhanced user engagement by enabling personalized product recommendations.",
+      "Leveraged React Context API for state management",
+      "Conceptualized React Router for navigation."
     ],
     skills: [
-      "JavaScript",
-      "ES6",
       "React",
-      "Redux",
-      "Tailwind",
-      "Git",
-      "UiPath",
-      "Microsoft Power Automate",
+      "Firebase",
+      "Authentication",
+      "Firestore",
+      "AI",
+      "Context API",
+      "React Router"
     ],
   },
   {
-    title: "In house training",
-    company_name: "AWS Academy",
-    icon: aws,
-    iconBg: "#fff",
-    date: "july 2022 - Sep 2022",
-    points: [
-      "Completed training provided by Medicaps University Indore, on Aws Academy Graduate and AWS Academy Cloud Foundation Courses.",
-      "Demonstrated practical understanding of cloud computing architecture, security best practices, and scalability principles through hands-on labs and exercises.",
-      `Acquired knowledge in AWS services such as Colud compute, Storage, Database, Networking, Security and Identity Services`,
-    ],
-    skills: [
-      "EC2",
-      "ECS",
-      "S3",
-      "EBS",
-      "RDS",
-      "MySQL",
-      "DynamoDB",
-      "VPC",
-      "IAM",
-    ],
-  },
-  {
-    title: "Internship Program",
-    company_name: "TCR Innovation, Navi Mumbai, Maharashtra",
-    icon: tcr,
+    title: "Library Management System - Full-Stack Application",
+    company_name: "Personal Project",
+    icon: library,
     iconBg: "#E6DEDD",
-    date: "Jan 2022 - April 2022",
+    date: "January 2024 - May 2024",
     points: [
-      "Worked as team lead and developer.",
-      "Developed Campus controler desktop based applications using Java and Structured Query Language.",
-      "Collaborating with team member to create effective and good looking application.",
+      "Innovative In-Memory Indexing: Developed a system to accelerate book retrieval speeds, eliminating slow sequential scans.",
+      "TypeScript and TDD Implementation: Integrated TypeScript and adopted Test-Driven Development, reducing code errors and post-deployment bugs.",
+      "RESTful APIs and Input Validation: Crafted APIs for efficient data flow and used Zod framework for server-side input validation.",
+      "Enhanced React Components: Improved functionality for real-time updates, boosting user interaction and experience.",
+      "Integrated Zod validation framework, fortifying server-side input validation.",
+      "MongoDB Integration and Process Streamlining: Optimized data storage with MongoDB and simplified library processes to reduce check-out/check-in times.",
+      "Orchestrated MongoDB integration, optimizing data storage and retrieval speeds through efficient CRUD operations.",
     ],
     skills: [
-      "Java",
-      "Swing",
-      "AWT",
-      "JDBC",
-      "ODBC",
-      "Abstract Classes",
-      "Interface",
-      "DBMS",
-      "Mysql",
+      "TypeScript",
+      "JavaScript",
+      "React",
+      "MongoDB",
+      "RESTful API",
+      "TDD",
+      "Zod"
     ],
   },
-  // {
-  //   title: "Web Developement Internship",
-  //   company_name: "Let's Grow More",
-  //   icon: lgm,
-  //   iconBg: "#383E56",
-  //   date: "Oct 2021 - Dec 2021",
-  //   points: [
-  //     "Completed two tasks",
-  //     "One is a simple single page application using the HTMl,CSS and Javascript",
-  //     "Another one is a multipage web application using the react and fetch api",
-  //   ],
-  //   skills: ["HTML", "HTML5", "CSS", "JavaScript", "React", "API"],
-  // },
+  {
+    title: "Game Street - React-based Online Game Store",
+    company_name: "Personal Project",
+    icon: gameStreet,
+    iconBg: "#383E56",
+    date: "September 2023 - November 2023",
+    points: [
+      "Engineered a sophisticated cart management system using Fetch API, streamlining the checkout process and reducing cart abandonment by 30%.",
+      "Leveraged JSON Server to mock RESTful API endpoints, accelerating development enabling rapid prototyping.",
+      "Simulated complex backend operations for game inventory, orders, and user data, reducing development time.",
+      "Crafted an intuitive order management dashboard, improving order tracking efficiency and enhancing customer satisfaction.",
+      "Implemented JWT-based authentication, bolstering user session security and reducing unauthorized access attempts.",
+      "Designed responsive UI components, ensuring seamless user experience across devices."
+    ],
+    skills: [
+      "React",
+      "Fetch API",
+      "JSON Server",
+      "RESTful API",
+      "JWT",
+      "Responsive Design"
+    ],
+  },
 ];
 
 const education = [
   {
-    title: "Medi-Caps University, Indore",
-    company_name: "B.Tech, CSE, 8.58CGPA",
+    title: "Binghamton University, State University Of New York",
+    company_name: "Master of Science in Computer Science",
     icon: circle,
     iconBg: "#383E56",
-    date: "Aug 2019 - June 2023",
-    point:
-      "I have graduated from this University where I have taken courses in Data Structures, Algorithms, Object-Oriented Programming, Database Management Systems, Operating Systems, Computer Networks, Computer Organizations, Data mining, and many more.",
+    date: "Present - June 2025",
+    point: "Currently pursuing a Master's degree in Computer Science. Received Dean's List: Watson college Scholarship for fall 2023. Relevant coursework includes Database Systems, OS, Data Structure and Algorithm, Programming languages, and Distributed Systems.",
   },
   {
-    title: "St. Anna HS. Convent School",
-    company_name: "XII, PCM, 88%",
+    title: "Medi-caps University",
+    company_name: "Bachelor of Technology in Computer Science",
     icon: circle,
     iconBg: "#383E56",
-    date: "Aug 2016 - June 2018",
-    point:
-      "I have completed my class 12th in this school where I took physics, chemistry, math and Secured second highest position in school in final board exams.",
+    date: "August 2019 - May 2023",
+    point: "Completed a Bachelor's degree in Computer Science. Coursework included programming languages (C, C++, JavaScript, TypeScript), web technologies (HTML, CSS, React, CSS Tailwind, Firebase), databases (MySQL, MongoDB), and other relevant technologies such as RESTful APIs, Git, JSON Server, Test-Driven Development (TDD), and Jest.",
   },
 ];
 
-const testimonials = [
-  {
-    testimonial:
-      "I've had the privilege of witnessing their journey as a software engineer. I have no doubt that Kundan will continue to excel in their career in the field of software engineering.",
-    name: "Sakshi Bhagwat",
-    designation: "SDE",
-    company: "Citi",
-    image: sakshi,
-  },
-  {
-    testimonial:
-      "As a close friend of him, Working with Kundan has been an absolute pleasure. Their dedication to quality making things and attention to detail are unmatched.",
-    name: "Sahil Samaiaya",
-    designation: "SDE",
-    company: "Valuelabs",
-    image: sahil,
-  },
-  {
-    testimonial:
-      "I've had the pleasure of knowing Kundan on both a personal and professional level, and I can confidently say that He is one of the most talented person I've ever met.",
-    name: "Urvi Dalal",
-    designation: "SDE",
-    company: "Valuelabs",
-    image: urvi,
-  },
-];
+
 
 const projects = [
   {
-    name: "Food Ordering Website",
+    name: "Petadopt - Full-Stack Web Application",
     description:
-      "Indulge your cravings with our seamless food ordering website built with React. Explore a diverse selection of cuisines, place orders effortlessly. With intuitive navigation and a user-friendly interface, satisfying your hunger has never been easier!",
+      "A comprehensive pet adoption platform featuring Firebase Authentication, AI-driven pet matching, real-time data management with Firestore, and an admin dashboard. The application includes a robust pet listing system with advanced search capabilities and an innovative shopping cart solution.",
     tags: [
       {
-        name: "react",
+        name: "React",
         color: "blue-text-gradient",
       },
       {
         name: "Firebase",
-        color: "white",
-      },
-      {
-        name: "tailwind",
-        color: "pink-text-gradient",
-      },
-    ],
-    image: food2,
-    source_code_link: "https://github.com/kundann760/Food-ordering-app",
-    project_live_link: "",
-  },
-
-  {
-    name: "Nike - Kundan's Outlet",
-    description:
-      "the live website showcasing my interpretation of Nike's Summer Collection! Built with React and styled with Tailwind CSS, this fully responsive website offers a seamless browsing experience for sneaker enthusiasts and design aficionados alike.",
-    tags: [
-      {
-        name: "React JS",
-        color: "pink-text-gradient",
-      },
-      {
-        name: "Tailwind Css",
         color: "green-text-gradient",
       },
+      {
+        name: "AI",
+        color: "pink-text-gradient",
+      },
     ],
-    image: nike,
-    source_code_link: "https://github.com/kundann760/Nike",
-    project_live_link: "https://kundanmokhale01.vercel.app/",
+    image: petadopt, // You'll need to import this image
+    source_code_link: "https://github.com/j27000/Petadopt",
+    project_live_link: "https://petadopt-xi.vercel.app/",
   },
   {
-    name: "Fake Shop",
+    name: "Library Management System",
     description:
-      "Discover a world of endless shopping possibilities at our FakeShop store, crafted with React technology. Browse through stunning product images. With just a click, effortlessly add your favorite items to your shopping cart. all powered by React.",
+      "A full-stack application implementing TypeScript and JavaScript, featuring an in-memory indexing scheme for efficient book retrieval. Utilizes Test-Driven Development, RESTful APIs, Zod validation, and MongoDB integration for optimized data storage and retrieval.",
     tags: [
       {
-        name: "react",
+        name: "TypeScript",
         color: "blue-text-gradient",
       },
       {
-        name: "tailwind",
-        color: "blue",
-      },
-      {
-        name: "restapi",
+        name: "MongoDB",
         color: "green-text-gradient",
       },
-
       {
-        name: "redux",
+        name: "React",
         color: "pink-text-gradient",
       },
     ],
-    image: fakeshop,
-    source_code_link: "https://github.com/kundann760/FakeShop",
-    project_live_link: "",
+    image: library, // You'll need to import this image
+    source_code_link: "https://github.com/j27000/LibManagement",
+    project_live_link: "https://github.com/j27000/LibManagement",
+  },
+  {
+    name: "Game Street - Online Game Store",
+    description:
+      "A React-based online game store featuring a sophisticated cart management system, JSON Server for API mocking, and an intuitive order management dashboard. Implements JWT-based authentication and responsive UI components for seamless user experience across devices.",
+    tags: [
+      {
+        name: "React",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "JSON Server",
+        color: "green-text-gradient",
+      },
+      {
+        name: "JWT",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: gameStreet, // You'll need to import this image
+    source_code_link: "https://github.com/j27000/gamestreet",
+    project_live_link: "https://gamestreet-lac.vercel.app/",
   },
 ];
 
@@ -420,7 +310,6 @@ export {
   otherTechnologies,
   tools,
   experiences,
-  testimonials,
   projects,
   education,
 };
